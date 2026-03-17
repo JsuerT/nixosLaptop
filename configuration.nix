a8b950599669438ac156b5294527ef0607ccc463
Jojo (Ticco) <johanna.suermann@iu-study.org>
2026-03-17 15:15:47 +0100

    vim
    Refactor vim configuration and add new plugins

--- configuration.nix
+++ configuration.nix
@@ -1,42 +1,18 @@
-# Edit this configuration file to define what should be installed 
-# on your system.  Help is available in the configuration.nix(5) 
-# man page and in the NixOS manual (accessible by running 
-# ‘nixos-help’).
-
-{ config, pkgs, ... }:
-
-
-{ imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix ];
-
- nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true; 
-  boot.loader.efi.canTouchEfiVariables = true;
-
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true; # Enables wireless support 
   # via wpa_supplicant.
-
   # Configure network proxy if necessary networking.proxy.default 
   # = "http://user:password@proxy:port/"; 
   # networking.proxy.noProxy = 
   # "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking.networkmanager.enable = true;
-
   # Enable network manager applet
   programs.nm-applet.enable = true;
-
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
-
   # Select internationalisation properties.
   i18n.defaultLocale = "en_GB.UTF-8";
-
   i18n.extraLocaleSettings = { LC_ADDRESS = "de_DE.UTF-8"; 
     LC_IDENTIFICATION = "de_DE.UTF-8"; LC_MEASUREMENT = 
     "de_DE.UTF-8"; LC_MONETARY = "de_DE.UTF-8"; LC_NAME = 
@@ -44,24 +20,18 @@
     "de_DE.UTF-8"; LC_TELEPHONE = "de_DE.UTF-8"; LC_TIME = 
     "de_DE.UTF-8";
   };
-
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-
   # Enable the LXQT Desktop Environment.
   services.desktopManager.plasma6.enable = true;
   services.displayManager.sddm.enable = true;
-
   # Configure keymap in X11
   services.xserver.xkb = { layout = "de"; variant = "";
   };
-
   # Configure console keymap
   console.keyMap = "de";
-
   # Enable CUPS to print documents.
   services.printing.enable = true;
-
   # Enable sound with pipewire.
   services.pulseaudio.enable = false; security.rtkit.enable = 
   true; services.pipewire = {
@@ -69,16 +39,13 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this 
     #jack.enable = true;
-
     # use the example session manager (no others are packaged yet 
     # so this is enabled by default, no need to redefine it in 
     # your config for now)
     #media-session.enable = true;
   };
-
   # Enable touchpad support (enabled default in most 
   # desktopManager). services.xserver.libinput.enable = true;
-
   # Define a user account. Don't forget to set a password with 
   # ‘passwd’.
   users.users.ticco = { isNormalUser = true; description = 
@@ -87,17 +54,13 @@
     #  thunderbird
     ];
   };
-
   # Install firefox.
   programs.firefox.enable = true;
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
   # List packages installed in system profile. To search, run: $ 
   # nix search wget
   
-
    environment.systemPackages = with pkgs; [
 openjdk21
 git
@@ -105,36 +68,61 @@ git
       name = "vim";
       vimrcConfig.packages.myVimPackage = {
         start = with pkgs.vimPlugins; [ 
-          vim-airline 
-          nerdtree 
-          vim-nix 
+                  vim-airline
+            vim-airline-themes
+            nerdtree
+            vim-fugitive
+            vim-gitgutter
+            vim-surround
+            vim-commentary
+            fzf-vim
+            gruvbox
+            vim-polyglot
+            indentLine
+            auto-pairs
         ];
       };
       # In der customRC ist es sauberer, Befehle untereinander zu schreiben
       vimrcConfig.customRC = ''
-        set number
-        set relativenumber
-        syntax on
-      '';
+             syntax on
+          set number
+          set relativenumber
+          set termguicolors
+          set encoding=utf-8
+          set shiftwidth=2
+          set expandtab
+          set mouse=a
+
+          " Theme Einstellungen
+          colorscheme gruvbox
+          let g:airline_theme='gruvbox'
+          let g:airline_powerline_fonts = 1
+
+          " Keybindings
+          let mapleader = " "
+
+          " Strg + n öffnet den Dateibaum (NERDTree)
+          nnoremap <C-n> :NERDTreeToggle<CR>
+
+          " Leertaste + ff sucht nach Dateien
+          nnoremap <leader>ff :Files<CR>
+          
+          " Leertaste + fg sucht in Git-Dateien
+          nnoremap <leader>fg :GFiles<CR>
+        '';
     })
   ];
-
-
   # Some programs need SUID wrappers, can be configured further 
   # or are started in user sessions. programs.mtr.enable = true; 
   # programs.gnupg.agent = {
   #   enable = true; enableSSHSupport = true;
   # };
-
   # List services that you want to enable:
-
   # Enable the OpenSSH daemon. services.openssh.enable = true;
-
   # Open ports in the firewall. 
   # networking.firewall.allowedTCPPorts = [ ... ]; 
   # networking.firewall.allowedUDPPorts = [ ... ]; Or disable the 
   # firewall altogether. networking.firewall.enable = false;
-
   # This value determines the NixOS release from which the 
   # default settings for stateful data, like file locations and 
   # database versions on your system were taken. It‘s perfectly 
@@ -144,5 +132,4 @@ git
   # configuration.nix or on 
   # https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
-
 }
